62d27eed04364c3788c50638da534af5294b05e2
Jacob Somerville <j.somerville97@gmail.com>
2024-09-08 01:54:47 +0930

    chore: layout formatting

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,24 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
+import Head from 'next/head';
 import localFont from "next/font/local";
 import "./globals.css";
 import "leaflet/dist/leaflet.css";
 
 const notoSans = localFont({
-  src: "./fonts/NotoSans-VariableFont_wdth,wght.ttf",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+    src: "./fonts/NotoSans-VariableFont_wdth,wght.ttf",
+    variable: "--font-geist-sans",
+    weight: "100 900",
 });
 const header = localFont({
-  src: "./fonts/DelaGothicOne-Regular.ttf",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+    src: "./fonts/DelaGothicOne-Regular.ttf",
+    variable: "--font-geist-mono",
+    weight: "100 900",
 });
 
 export const metadata: Metadata = {
-  title: "Croc-Spot",
-  description: "Crowdsourced crocodile sightings and alerts",
+    title: "Croc-Spot",
+    description: "Crowdsourced crocodile sightings and alerts",
+};
 
 export const viewport: Viewport = {
     viewportFit: "cover",
@@ -30,13 +32,13 @@ export default function RootLayout({
 }: Readonly<{
     children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${notoSans.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body
+                className={`${notoSans.variable} antialiased`}
+            >
+                {children}
+            </body>
+        </html>
+    );
 }
